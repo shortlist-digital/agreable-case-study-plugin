2f95d3e01a3b9cd84f3bcb684e4e564df0052716
dan_adeyemi <iamdeaneyelid@gmail.com>
2016-08-19 10:20:27 +0100

    header hooking to base theme

--- app/CustomFields/header.php
+++ app/CustomFields/header.php
@@ -5,6 +5,6 @@ $post_type = 'casestudy';
 
 $header_acf = HeaderDefinition::get($post_type);
 
-$header_acf = apply_filters('agreable_partnership_plugin_header_acf', $header_acf);
+$header_acf = apply_filters('agreable_base_theme_header_acf', $header_acf);
 
 register_field_group($header_acf);
