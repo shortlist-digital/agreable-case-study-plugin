126eac0818755b0289dbdbfdb64a2f39d2757278
Kimberley Cook <kimberleycook91@gmail.com>
2016-09-28 12:38:44 +0200

    related content hook added

---
+++ app/Hooks/RelatedContentAcf.php
@@ -0,0 +1,19 @@
+<?php namespace AgreableCaseStudyPlugin\Hooks;
+
+class RelatedContentAcf {
+  public function init() {
+    \add_filter('agreable_base_theme_related_acf', array($this, 'related_acf'), 10);
+  }
+
+  public function related_acf($acf_definition) {
+    $acf_definition['location'][] = [
+      [
+        'param' => 'post_type',
+        'operator' => '==',
+        'value' => 'case_study',
+      ]
+    ];
+
+    return $acf_definition;
+  }
+}
